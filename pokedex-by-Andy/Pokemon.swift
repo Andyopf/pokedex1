fa61577e4fbd5bba98c634670b426474a61efa70
Anan Lappikulthong <ananlappikulthong@Anans-MacBook-Air.local>
2016-12-03 02:14:24 +0700

    update Dec, 3, 2016 2,14am

---
+++ pokedex-by-Andy/Pokemon.swift
@@ -0,0 +1,26 @@
+//
+//  Pokemon.swift
+//  pokedex-by-Andy
+//
+//  Created by Anan Lappikulthong on 12/3/2559 BE.
+//  Copyright © 2559 Anan Lappikulthong. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
--- pokedex-by-Andy/ViewController.swift
+++ pokedex-by-Andy/ViewController.swift
@@ -12,13 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var pokemon = Pokemon(name: "Charizard", pokedexId: 6)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
